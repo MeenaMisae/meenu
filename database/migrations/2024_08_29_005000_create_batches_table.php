0d83dcaa03496ad89ffee30b1f75294bc0fd5f89
MeenaMisae <meenahiwatashi@gmail.com>
2024-08-28 21:55:25 -0300

    feat: migration de lote

---
+++ database/migrations/2024_08_29_005000_create_batches_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('batches', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('recipe_id');
+            $table->integer('quantity_produced');
+            $table->date('production_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('batches');
+    }
+};
