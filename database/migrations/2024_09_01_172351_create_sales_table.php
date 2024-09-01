7965dd98a3af501457ec3d71ee53c587a862e4de
MeenaMisae <meenahiwatashi@gmail.com>
2024-09-01 14:35:46 -0300

    feat:  adiciona migration de vendas

---
+++ database/migrations/2024_09_01_172351_create_sales_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('batch_id');
+            $table->integer('quantity_sold');
+            $table->decimal('sale_price');
+            $table->decimal('total_revenue');
+            $table->date('sale_date');
+            $table->foreign('batch_id')->references('id')->on('batches')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
