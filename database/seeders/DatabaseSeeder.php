a2bf9e8fab7fe6a18708940a3a0344f753d7713e
MeenaMisae <meenahiwatashi@gmail.com>
2024-09-01 17:48:09 -0300

    chore: adiciona login ao seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
+            'login' => 'test',
             'email' => 'test@example.com',
         ]);
     }
