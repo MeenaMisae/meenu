ac0402728d937e2644b08cde844b900e2541d465
MeenaMisae <meenahiwatashi@gmail.com>
2024-09-01 21:34:30 -0300

    feat: altera cor do checkbox

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,8 @@
 <x-guest-layout>
-    <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
     <form method="POST" action="{{ route('login') }}">
         @csrf
-        <!-- Email Address -->
         <div>
             <x-input-label for="login" :value="'Login'" />
             <x-text-input id="login" class="block w-full mt-1" type="text" name="login" :value="old('login')" required
@@ -12,7 +10,6 @@
             <x-input-error :messages="$errors->get('login')" class="mt-2" />
         </div>
 
-        <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
@@ -22,11 +19,10 @@
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox"
-                    class="text-indigo-600 border-gray-300 rounded shadow-sm focus:ring-indigo-500" name="remember">
+                    class="text-[#474747] border-gray-300 rounded shadow-sm focus:ring-[#ababab]" name="remember">
                 <span class="text-sm text-gray-600 ms-2">{{ __('Remember me') }}</span>
             </label>
         </div>
