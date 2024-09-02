c73fd99c3e2905b01783e129dd038a93ad58c942
MeenaMisae <meenahiwatashi@gmail.com>
2024-09-01 21:35:36 -0300

    feat: altera fonte do layout de convidados

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -8,18 +8,19 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Anek+Telugu:wght@100..800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Afacad:ital,wght@0,400..700;1,400..700&display=swap"
+        rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="font-sans antialiased text-gray-900">
-    <div class="flex flex-col items-center min-h-screen pt-6 bg-gray-100 sm:justify-center sm:pt-0">
+    <div class="flex flex-col items-center min-h-screen pt-6 bg-[#3b3b3b] sm:justify-center sm:pt-0">
         <div>
             <a href="/">
-                <x-application-logo class="w-40 h-40 text-gray-500 fill-current" />
+                <x-application-logo class="w-40 h-12 text-gray-500 fill-current" />
             </a>
         </div>
-        <div class="w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:max-w-md sm:rounded-lg">
+        <div class="w-full px-6 py-4 mt-6 overflow-hidden bg-[#dadada] shadow-md sm:max-w-md sm:rounded-lg">
             {{ $slot }}
         </div>
     </div>
