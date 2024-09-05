943389907231a832a11d92d0ad95c61f7d2a5971
MeenaMisae <meenahiwatashi@gmail.com>
2024-09-05 20:09:43 -0300

    feat: construcao da pagina de receitas

---
+++ resources/views/livewire/admin/recipes/index.blade.php
@@ -0,0 +1,6 @@
+<x-app-layout>
+    <x-navigation :title="'Receitas'" />
+    <div class="flex items-center justify-center h-[80vh]">
+        <h1 class="text-2xl font-bold">Receitas em construção 🚧</h1>
+    </div>
+</x-app-layout>
