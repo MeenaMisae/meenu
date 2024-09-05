43c633ec0257a87899196af0075501ad98d7bb32
MeenaMisae <meenahiwatashi@gmail.com>
2024-09-05 20:12:25 -0300

    feat: componente de nav no dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+    <x-navigation :title="'Painel'" />
     <div class="flex items-center justify-center h-[80vh]">
         <h1 class="text-2xl font-bold">Painel em construção 🚧</h1>
     </div>
