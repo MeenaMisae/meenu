c939728b9f0817e14fc66e0dba95a2dc71f20a46
MeenaMisae <meenahiwatashi@gmail.com>
2024-09-05 20:09:31 -0300

    feat: construcao da pagina de nova venda

---
+++ resources/views/livewire/admin/sales/create.blade.php
@@ -0,0 +1,6 @@
+<x-app-layout>
+    <x-navigation :title="'Nova venda'" />
+    <div class="flex items-center justify-center h-[80vh]">
+        <h1 class="text-2xl font-bold">Nova venda em construção 🚧</h1>
+    </div>
+</x-app-layout>
