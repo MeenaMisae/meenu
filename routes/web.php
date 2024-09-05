628447629de9e60a09d6f22a331a46bbca86f0d2
MeenaMisae <meenahiwatashi@gmail.com>
2024-09-05 20:09:13 -0300

    feat: adiciona novas rotas

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,26 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Admin\Recipes\Index as RecipesIndex;
+use App\Livewire\Admin\Sales\Create;
+use App\Livewire\Admin\Sales\Index as SalesIndex;
 
 Route::get('/', function () {
     return redirect('dashboard');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::prefix('admin')->middleware(['auth'])->group(function () {
+    Route::get('painel', function () {
+        return view('dashboard');
+    })->name('admin.dashboard.index');
+    Route::prefix('receitas')->group(function () {
+        Route::get('', RecipesIndex::class)->name('admin.recipes.index');
+    });
+    Route::prefix('vendas')->group(function () {
+        Route::get('', SalesIndex::class)->name('admin.sales.index');
+        Route::get('nova-venda', Create::class)->name('admin.sales.create');
+    });
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
