f62d71caccbb22d0a20f5ee506a3eb234e326a65
MeenaMisae <meenahiwatashi@gmail.com>
2024-09-06 19:19:33 -0300

    feat: adiciona rota pro estoque

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,10 @@ use Illuminate\Support\Facades\Route;
 use App\Livewire\Admin\Recipes\Index as RecipesIndex;
 use App\Livewire\Admin\Sales\Create;
 use App\Livewire\Admin\Sales\Index as SalesIndex;
+use App\Livewire\Admin\Stock\Index as StockIndex;
 
 Route::get('/', function () {
-    return redirect('dashboard');
+    return redirect()->route('admin.dashboard.index');
 });
 
 Route::prefix('admin')->middleware(['auth'])->group(function () {
@@ -20,6 +21,9 @@ Route::prefix('admin')->middleware(['auth'])->group(function () {
     Route::prefix('vendas')->group(function () {
         Route::get('', SalesIndex::class)->name('admin.sales.index');
         Route::get('nova-venda', Create::class)->name('admin.sales.create');
+    });
+    Route::prefix('estoque')->group(function () {
+        Route::get('', StockIndex::class)->name('admin.stock.index');
     });
 });
 
