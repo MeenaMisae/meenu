39b8bdfcfbd54cf203b15450093d71d746532ea5
MeenaMisae <meenahiwatashi@gmail.com>
2024-09-06 19:19:58 -0300

    feat: construção da pagina de estoque

---
+++ resources/views/livewire/admin/stock/index.blade.php
@@ -0,0 +1,6 @@
+<x-app-layout>
+    <x-navigation :title="'Estoque'" />
+    <div class="flex items-center justify-center h-[80vh]">
+        <h1 class="text-2xl font-bold">Estoque em construção 🚧</h1>
+    </div>
+</x-app-layout>
