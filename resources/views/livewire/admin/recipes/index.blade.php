3ea6ab881903eee4d3f0500944401db4f468c62a
MeenaMisae <meenahiwatashi@gmail.com>
2024-09-07 17:03:05 -0300

    chore: retira x-layout desnecessario

--- resources/views/livewire/admin/recipes/index.blade.php
+++ resources/views/livewire/admin/recipes/index.blade.php
@@ -1,6 +1,3 @@
-<x-app-layout>
-    <x-navigation :title="'Receitas'" />
-    <div class="flex items-center justify-center h-[80vh]">
-        <h1 class="text-2xl font-bold">Receitas em construção 🚧</h1>
-    </div>
-</x-app-layout>
+<div class="flex items-center justify-center h-[80vh]">
+    <h1 class="text-2xl font-bold">Receitas em construção 🚧</h1>
+</div>
