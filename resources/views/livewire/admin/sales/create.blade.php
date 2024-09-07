8eecfd74bdd049689406fad72df00236e5892111
MeenaMisae <meenahiwatashi@gmail.com>
2024-09-07 17:03:17 -0300

    chore: retira x-layout desnecessario

--- resources/views/livewire/admin/sales/create.blade.php
+++ resources/views/livewire/admin/sales/create.blade.php
@@ -1,6 +1,3 @@
-<x-app-layout>
-    <x-navigation :title="'Nova venda'" />
-    <div class="flex items-center justify-center h-[80vh]">
-        <h1 class="text-2xl font-bold">Nova venda em construção 🚧</h1>
-    </div>
-</x-app-layout>
+<div class="flex items-center justify-center h-[80vh]">
+    <h1 class="text-2xl font-bold">Nova venda em construção 🚧</h1>
+</div>
