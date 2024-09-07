5b5468a9f3da4f7f2acfbe275d66d1e7b820aa7f
MeenaMisae <meenahiwatashi@gmail.com>
2024-09-07 16:58:23 -0300

    feat: seletor de tema

---
+++ app/Livewire/ThemeSelector.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ThemeSelector extends Component
+{
+    public function render()
+    {
+        return view('livewire.theme-selector');
+    }
+}
