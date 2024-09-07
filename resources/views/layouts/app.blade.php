9d7e84f6bbe3a8663515050170b5996f6d78b9f6
MeenaMisae <meenahiwatashi@gmail.com>
2024-09-07 17:02:29 -0300

    feat: recupera tema do local storage

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,26 +1,29 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="light">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="" class="font-afacad">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Afacad:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
-
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans antialiased">
+<body>
     <div class="min-h-screen bg-gray-100">
-        <div class="drawer"></div>
+        <div class="drawer">
+            <x-navigation />
+        </div>
         <main>
             {{ $slot }}
         </main>
     </div>
 </body>
+<script>
+    document.addEventListener('livewire:navigated', () => {
+        const theme = localStorage.getItem('theme') || 'dark'
+        document.querySelector('html').setAttribute('data-theme', theme);
+    })
+</script>
 
 </html>
