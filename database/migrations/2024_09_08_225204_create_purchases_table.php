222a60d02aee8174e040260aa5ac555c41c30a40
MeenaMisae <meenahiwatashi@gmail.com>
2024-09-08 20:22:11 -0300

    feat: migration de compras

---
+++ database/migrations/2024_09_08_225204_create_purchases_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('ingredient_id');
+            $table->decimal('quantity_purchased');
+            $table->decimal('price_per_unit');
+            $table->date('purchase_date');
+            $table->foreign('ingredient_id')->references('id')->on('ingredients')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ingredient_purchases');
+    }
+};
