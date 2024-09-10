dc39b66873c840c1580354b1545bc768b2531f59
MeenaMisae <meenahiwatashi@gmail.com>
2024-09-09 21:14:12 -0300

    feat: metodo que cria item no estoque

--- app/Livewire/Admin/Stock/Create.php
+++ app/Livewire/Admin/Stock/Create.php
@@ -2,13 +2,34 @@
 
 namespace App\Livewire\Admin\Stock;
 
+use App\Models\Ingredient;
+use Illuminate\Contracts\View\View;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Create extends Component
 {
-    public bool $boughtToday = true;
-    public function render()
+    #[Validate('required|string|min:2|max:24')]
+    public string $name;
+    #[Validate('required|numeric|min:0', as: 'quantidade')]
+    public string $quantity;
+    #[Validate('required|in:g,ml,each,kg,l', as: 'unidade')]
+    public string $unit = '';
+
+    public function render(): View
     {
         return view('livewire.admin.stock.create');
+    }
+
+    public function save(): void
+    {
+        $this->validate();
+        Ingredient::create([
+            'name' => $this->name,
+            'quantity_in_stock' => $this->quantity,
+            'unit' => $this->unit
+        ]);
+        $this->js('add_stock_modal.close()');
+        $this->dispatch('created');
     }
 }
