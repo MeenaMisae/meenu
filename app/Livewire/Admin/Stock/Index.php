8277dea13ff75d47d6e93f9744035bed3079b271
MeenaMisae <meenahiwatashi@gmail.com>
2024-09-09 21:14:26 -0300

    feat: lista os itens do estoque

--- app/Livewire/Admin/Stock/Index.php
+++ app/Livewire/Admin/Stock/Index.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Admin\Stock;
 
+use App\Models\Ingredient;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
@@ -10,6 +11,8 @@ class Index extends Component
     #[Layout('layouts.app')]
     public function render()
     {
-        return view('livewire.admin.stock.index');
+        return view('livewire.admin.stock.index', [
+            'ingredients' => Ingredient::all()
+        ]);
     }
 }
