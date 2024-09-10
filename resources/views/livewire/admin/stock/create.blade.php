aff58127fdfc0762ff1ba72ce4c1a09ac2a0dce0
MeenaMisae <meenahiwatashi@gmail.com>
2024-09-09 21:16:05 -0300

    feat: add mensagens de erro na view

--- resources/views/livewire/admin/stock/create.blade.php
+++ resources/views/livewire/admin/stock/create.blade.php
@@ -1,24 +1,38 @@
 <dialog id="add_stock_modal" class="modal" wire:ignore.self>
     <div class="modal-box">
-        <form class="flex flex-col h-full gap-y-4">
+        <form wire:submit="save" class="flex flex-col h-full gap-y-4">
             <div class="form-control">
                 <label for="name" class="label">Nome:</label>
-                <input wire:model="name" type="text" class="input input-bordered" id="name" placeholder="Manteiga"
-                    required>
+                <input wire:model="name" type="text" class="input input-bordered" id="name"
+                    placeholder="Manteiga">
+                <div class="h-2">
+                    @error('name')
+                        <span class="text-error font-semibold text-lg">{{ $message }}</span>
+                    @enderror
+                </div>
             </div>
             <div>
                 <label for="quantity" class="label">Quantidade:</label>
                 <div class="join">
                     <input wire:model="quantity" type="number" class="w-full input input-bordered join-item"
-                        id="quantity" step="0.01" min="0" placeholder="1" required>
-                    <select wire:model="unit" name="unit" id="unit"
-                        class="w-1/4 select select-bordered join-item" required>
+                        id="quantity" step="0.01" min="0" placeholder="1">
+                    <select wire:model.live="unit" name="unit" id="unit"
+                        class="w-1/4 select select-bordered join-item">
+                        <option value="" selected disabled></option>
                         <option value="g">g</option>
                         <option value="ml">ml</option>
-                        <option value="unit">unidade</option>
+                        <option value="each">unidade</option>
                         <option value="kg">kg</option>
                         <option value="l">l</option>
                     </select>
+                </div>
+                <div class="flex flex-col">
+                    @error('quantity')
+                        <span class="text-error font-semibold text-lg">{{ $message }}</span>
+                    @enderror
+                    @error('unit')
+                        <span class="text-error font-semibold text-lg">{{ $message }}</span>
+                    @enderror
                 </div>
             </div>
             {{-- <label for="itemName" class="label">Valor:</label>
