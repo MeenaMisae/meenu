f9c707d8df3edf39fad809eef159b48061b30e50
MeenaMisae <meenahiwatashi@gmail.com>
2024-09-09 21:15:28 -0300

    chore: exclui campo da migration

--- database/migrations/2024_08_29_001057_create_ingredients_table.php
+++ database/migrations/2024_08_29_001057_create_ingredients_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('name');
             $table->decimal('quantity_in_stock');
             $table->string('unit');
-            $table->decimal('cost_per_unit');
             $table->timestamps();
         });
     }
