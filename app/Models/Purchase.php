09c5365990aff87c6d7c4f8e519b88a6e875f4ad
MeenaMisae <meenahiwatashi@gmail.com>
2024-09-11 22:11:44 -0300

    feat: adiciona campos

--- app/Models/Purchase.php
+++ app/Models/Purchase.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Purchase extends Model
 {
+    protected $fillable = ['ingredient_id', 'quantity_purchased', 'price_per_unit', 'purchase_date'];
     use HasFactory;
+    public function ingredient(): BelongsTo
+    {
+        return $this->belongsTo(Ingredient::class);
+    }
 }
