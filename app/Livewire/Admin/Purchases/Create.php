6eca148848f795fbc4a25607c2c87dd859b713e8
MeenaMisae <meenahiwatashi@gmail.com>
2024-09-11 22:11:13 -0300

    feat: propriedades de registro da compra

--- app/Livewire/Admin/Purchases/Create.php
+++ app/Livewire/Admin/Purchases/Create.php
@@ -2,12 +2,26 @@
 
 namespace App\Livewire\Admin\Purchases;
 
+use App\Models\Ingredient;
+use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class Create extends Component
 {
-    public function render()
+    public string $quantity;
+    public string $unit;
+    public bool $boughtToday = true;
+    public string $price;
+
+    public function render(): View
     {
-        return view('livewire.admin.purchases.create');
+        return view('livewire.admin.purchases.create', [
+            'items' => Ingredient::all()
+        ]);
+    }
+
+    public function loadUnit(Ingredient $item): void
+    {
+        $this->unit = $item->unit;
     }
 }
