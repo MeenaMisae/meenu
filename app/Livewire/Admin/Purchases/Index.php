17f777713573a45540f4f7ae52108618a0998dde
MeenaMisae <meenahiwatashi@gmail.com>
2024-09-19 21:05:17 -0300

    feat: agrupa as compras

--- app/Livewire/Admin/Purchases/Index.php
+++ app/Livewire/Admin/Purchases/Index.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Admin\Purchases;
 
+use App\Models\Purchase;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
@@ -10,6 +11,8 @@ class Index extends Component
     #[Layout('layouts.app')]
     public function render()
     {
-        return view('livewire.admin.purchases.index');
+        return view('livewire.admin.purchases.index', [
+            'purchases' => Purchase::get()->unique('ingredient_id')
+        ]);
     }
 }
