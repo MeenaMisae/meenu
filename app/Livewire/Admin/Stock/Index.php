f719025117554cc35973e7f18a5a447a04bd3c2c
MeenaMisae <meenahiwatashi@gmail.com>
2024-09-22 14:48:09 -0300

    feat: método que dispara evento para exibir modal de ediçao

--- app/Livewire/Admin/Stock/Index.php
+++ app/Livewire/Admin/Stock/Index.php
@@ -3,16 +3,22 @@
 namespace App\Livewire\Admin\Stock;
 
 use App\Models\Ingredient;
+use Illuminate\View\View;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Index extends Component
 {
     #[Layout('layouts.app')]
-    public function render()
+    public function render(): View
     {
         return view('livewire.admin.stock.index', [
             'ingredients' => Ingredient::all()
         ]);
+    }
+
+    public function showEditModal(Ingredient $ingredient): void
+    {
+        $this->dispatch('showEditModal', ingredient: $ingredient)->to(Edit::class);
     }
 }
