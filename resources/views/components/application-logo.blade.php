8edc7699f622cd9c3e645322e43e77bf6aeda0ba
MeenaMisae <meenahiwatashi@gmail.com>
2024-09-23 21:06:35 -0300

    feat: atributos da logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,6 +1,8 @@
 <div class="flex items-center gap-x-1">
-    <h1 class="text-3xl font-semibold tracking-wide">meenu</h1>
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mt-2 size-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+    <h1 {{ $attributes->merge(['class' => 'text-3xl font-semibold tracking-wide']) }}>meenu</h1>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+        class="mt-2 size-6">
+        <path stroke-linecap="round" stroke-linejoin="round"
+            d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
     </svg>
 </div>
