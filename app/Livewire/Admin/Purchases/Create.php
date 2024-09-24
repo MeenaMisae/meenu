1fab0c7a1781b2f4724979a634055cc0fc0cbca0
MeenaMisae <meenahiwatashi@gmail.com>
2024-09-23 21:05:49 -0300

    chore: corrige atributos da classe

--- app/Livewire/Admin/Purchases/Create.php
+++ app/Livewire/Admin/Purchases/Create.php
@@ -6,14 +6,18 @@ use App\Models\Ingredient;
 use App\Models\Purchase;
 use Illuminate\Contracts\View\View;
 use Livewire\Component;
+use Livewire\Attributes\Validate;
 
 class Create extends Component
 {
-    public string $quantity;
+    #[Validate('required|numeric|min:0', as: 'quantidade')]
+    public float $quantity;
     public string $unit;
     public bool $boughtToday = true;
-    public string $price;
+    #[Validate('required|numeric|min:0', as: 'preço')]
+    public float $price;
     public string $purchaseDate;
+    #[Validate('required|exists:ingredients,id', as: 'item')]
     public int $ingredientId = 0;
     public Ingredient $ingredient;
 
@@ -42,6 +46,6 @@ class Create extends Component
         $this->ingredient->quantity_in_stock = $this->ingredient->quantity_in_stock + $this->quantity;
         $this->ingredient->save();
         $this->js('add_purchase_modal.close()');
-        $this->dispatch('created');
+        $this->dispatch('created')->self();
     }
 }
