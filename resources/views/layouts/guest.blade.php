3422738b3c376bdb3c8e9e86ec0523a05e256195
MeenaMisae <meenahiwatashi@gmail.com>
2024-09-23 21:08:28 -0300

    feat: modifica layout do convidado

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="light">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="light" class="font-poppins">
 
 <head>
     <meta charset="utf-8">
@@ -13,16 +13,9 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans antialiased text-gray-900">
-    <div class="flex flex-col items-center min-h-screen pt-6 bg-[#3b3b3b] sm:justify-center sm:pt-0">
-        <div>
-            <a href="/">
-                <x-application-logo class="w-40 h-12 text-gray-500 fill-current" />
-            </a>
-        </div>
-        <div class="w-full px-6 py-4 mt-6 overflow-hidden bg-[#dadada] shadow-md sm:max-w-md sm:rounded-lg">
-            {{ $slot }}
-        </div>
+<body class="flex justify-center items-center h-screen">
+    <div>
+        {{ $slot }}
     </div>
 </body>
 
